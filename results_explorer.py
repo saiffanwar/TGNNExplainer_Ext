e61a52c5de02191b1d051e7cd776aa69ef7ab88d
Saif Anwar <anwarsaif99@hotmail.com>
2025-01-25 13:56:12 +0000

    updated resutls explorer

--- results_explorer.py
+++ results_explorer.py
@@ -7,101 +7,57 @@ import matplotlib.pyplot as plt
 #data_dir = 'tgnnexplainer/src/dataset/data/'
 #
 #data = pd.read_csv(data_dir + 'wikipedia.csv')
+fig, axes = plt.subplots(2, 2, figsize=(8, 7))
 
-combined_tgnne_results = None
-for i in [1,2,3,4,5,15,17,20]:
-    with open(f'tgnnexplainer/benchmarks/results/tgnne_results_{i}_wikipedia_tgn.pkl', 'rb') as file:
-        tgnne_results = pck.load(file)
-        sub_keys = list(tgnne_results.values())[0].keys()
-        if combined_tgnne_results is None:
-            combined_tgnne_results = {k:{k_: [] for k_ in sub_keys} for k in tgnne_results.keys()}
-        for k in tgnne_results.keys():
-            for k_ in sub_keys:
-                combined_tgnne_results[k][k_].extend(tgnne_results[k][k_])
+dataset = 'wikipedia'
+models = ['tgn', 'tgat']
+methods = ['STX-Search', 'TGNNExplainer', 'Temp-ME']#
+
+for m, model in enumerate(models):
+    combined_tgnne_results = None
+    if model == 'tgn':
+        result_batches = [1,2,3,4,5,15,17,20]
+    elif model == 'tgat':
+        result_batches = [1,2,3,4,5]
+#for i in [1,2,3,4,5,15,17,20]:
+    for i in result_batches:
+        with open(f'tgnnexplainer/benchmarks/results/tgnne_results_{dataset}_{model}_{i}.pkl', 'rb') as file:
+            tgnne_results = pck.load(file)
+            print(tgnne_results)
+            sub_keys = list(tgnne_results.values())[0].keys()
+            if combined_tgnne_results is None:
+                combined_tgnne_results = {k:{k_: [] for k_ in sub_keys} for k in tgnne_results.keys()}
+            for k in tgnne_results.keys():
+                for k_ in sub_keys:
+                    combined_tgnne_results[k][k_].extend(tgnne_results[k][k_])
+    tgnne_results = combined_tgnne_results
+#    print(tgnne_results)
+
+    with open(f'tgnnexplainer/benchmarks/results/sa_results_{dataset}_{model}_exp_sizes.pkl', 'rb') as file:
+        sa_results = pck.load(file)
+
+    with open(f'tgnnexplainer/benchmarks/results/temp_me_results_{dataset}_{model}_exp_sizes.pkl', 'rb') as file:
+        temp_me_results = pck.load(file)
 
 
-with open(f'tgnnexplainer/benchmarks/results/sa_results_wikipedia_tgn_exp_sizes.pkl', 'rb') as file:
-    sa_results = pck.load(file)
-
-with open(f'tgnnexplainer/benchmarks/results/temp_me_results_wikipedia_tgat_exp_sizes.pkl', 'rb') as file:
-    temp_me_results = pck.load(file)
-
-
-sa_errors = []
-sa_sizes = []
-sa_delta_fidelities = []
-print('------- STX-Search --------')
-for k in sa_results.keys():
-#    for i in range(len(sa_results[k]['target_event_idxs'])):
-#        sa_errors.append(abs(sa_results[k]['model_predictions'][i] - sa_results[k]['explanation_predictions'][i]))
-#        sa_sizes.append(len(sa_results[k]['explanations'][i]))
-    errors = [abs(y-y_hat) for y, y_hat in zip(sa_results[k]['model_predictions'], sa_results[k]['explanation_predictions'])]
-#    errors = errors[:-3]
-    sa_error = np.mean(errors)
-    print('Exp Size: ', k, 'Avg Error', sa_error)
-    sa_errors.append(sa_error)
-    sa_delta_fidelities.append(np.mean(sa_results[k]['delta_fidelity']))
-#    combined_sa_results[k].append(sa_results[k][])
-
-tgnne_results = combined_tgnne_results
-print(tgnne_results.keys())
-tgnne_errors = []
-tgnne_sizes = []
-tgnne_delta_fidelities = []
-print('------- TGNNExplainer --------')
-for k in tgnne_results.keys():
-
-#    for i in range(len(tgnne_results[k]['target_event_idxs'])):
-#        tgnne_errors.append(abs(tgnne_results[k]['model_predictions'][i] - tgnne_results[k]['explanation_predictions'][i]))
-#        tgnne_sizes.append(len(tgnne_results[k]['explanations'][i]))
 #
-    tgnne_error = np.mean([abs(y-y_hat) for y, y_hat in zip(tgnne_results[k]['model_predictions'], tgnne_results[k]['explanation_predictions'])])
-    print('Exp Size: ', k, 'Avg Error', tgnne_error)
-    tgnne_errors.append(float(tgnne_error))
-    tgnne_delta_fidelities.append(np.mean(tgnne_results[k]['delta_fidelity']))
 
+    colors = ['r', 'g', 'b']
+    for i, (result, method) in enumerate(zip([sa_results, tgnne_results, temp_me_results], methods)):
+        print('-------', method, '--------')
+        errors = []
+        sizes = []
+        delta_fidelities = []
+        for k in result.keys():
+            errors.append(np.mean([abs(y-y_hat) for y, y_hat in zip(result[k]['model_predictions'], result[k]['explanation_predictions'])]))
+            sizes.append(k)
+            delta_fidelities.append(np.mean(result[k]['delta_fidelity']))
+            print('Exp Size:', k, 'Avg Error:', errors[-1], 'Avg Delta Fidelity:', delta_fidelities[-1])
+        axes[0][m].plot(sizes, errors, color=colors[i])
+        axes[1][m].plot(sizes, delta_fidelities, color=colors[i])
 
+axes[0][0].set_title('TGN - Wikipedia')
+axes[0][1].set_title('TGAT - Wikipedia')
 
-print('------- Temp-ME --------')
-temp_me_errors = []
-temp_me_sizes = []
-temp_me_delta_fidelities = []
-for k in temp_me_results.keys():
-    temp_me_error = np.mean([abs(y-y_hat) for y, y_hat in zip(temp_me_results[k]['model_predictions'], temp_me_results[k]['explanation_predictions'])])
-    print('Exp Size: ', k, 'Avg Error', temp_me_error)
-    temp_me_errors.append(temp_me_error)
-    temp_me_delta_fidelities.append(np.mean(temp_me_results[k]['delta_fidelity']))
-
-fig, axes = plt.subplots(2, 3)
-#
-axes[0][0].set_title('STX-Search')
-axes[0][1].set_title('TGNNExplainer')
-axes[0][2].set_title('Temp-ME')
-axes[0][0].set_xlabel('Gamma (Exp Size Penalty)')
-axes[0][1].set_xlabel('Exp Size')
-axes[0][2].set_xlabel('Exp Size')
-axes[0][0].set_ylabel('Avg Error')
-axes[0][1].set_ylabel('Avg Error')
-axes[0][2].set_ylabel('Avg Error')
-axes[0][0].plot(sa_results.keys(), sa_errors, label='STX-Search')
-axes[0][1].plot(tgnne_results.keys(), tgnne_errors, label='SA')
-axes[0][2].plot(temp_me_results.keys(), temp_me_errors, label='TGNNE')
-
-axes[1][0].set_title('STX-Search')
-axes[1][1].set_title('TGNNExplainer')
-axes[1][2].set_title('Temp-ME')
-axes[1][0].set_xlabel('Gamma (Exp Size Penalty)')
-axes[1][1].set_xlabel('Exp Size')
-axes[1][2].set_xlabel('Exp Size')
-axes[1][0].set_ylabel('Avg Delta Fidelity')
-axes[1][1].set_ylabel('Avg Delta Fidelity')
-axes[1][2].set_ylabel('Avg Delta Fidelity')
-axes[1][0].plot(sa_results.keys(), sa_delta_fidelities, label='STX-Search')
-axes[1][1].plot(tgnne_results.keys(), tgnne_delta_fidelities, label='SA')
-axes[1][2].plot(temp_me_results.keys(), temp_me_delta_fidelities, label='TGNNE')
-
-#plt.scatter([, sa_errors, label='STX-Search')
-#plt.scatter(tgnne_sizes, tgnne_errors, label='SA')
-#plt.plot([10, 20, 30], tgnne_errors, label='TGNNE')
-plt.legend()
-plt.show()
+fig.legend(methods, loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.0))
+fig.savefig(f'Figures/{dataset}_results.png')
